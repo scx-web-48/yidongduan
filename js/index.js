2827d6ffd7aa524224136c313ef48e46c050707d
UBANTU <hansai1026@outlook.com>
2019-04-28 11:47:49 +0800

    完成

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ var swiper = new Swiper('.swiper-container', {
   spaceBetween: 15,
   freeMode: true,
 });
+$(".btn-clear").click(function () {
+  $(this).parent().find('input').val('');
+});
+$(".form-inp").click(function() {
+  $(this).find('input').focus();
+})
